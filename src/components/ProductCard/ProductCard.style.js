a2803866b8cb40269cce3358fa4d8e89c6aef048
Sude Sevim <sudesevim0@gmail.com>
2024-08-21 14:35:48 +0300

    added custom hook

---
+++ src/components/ProductCard/ProductCard.style.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        backgroundColor: '#e0e0e0',
+        borderColor: '#bdbdbd',
+        borderWidth: 1,
+        margin: 10,
+        flexDirection: 'row',
+
+    },
+    image: {
+        width: 100,
+        minHeight: 100,
+        resizeMode: 'contain',
+        backgroundColor: 'white',
+    },
+    body_container: {
+        flex: 1,
+        padding: 5,
+        justifyContent: 'space-between',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    price: {
+        textAlign: 'right',
+        fontSize: 14,
+        fontStyle: 'italic',
+    },
+});
--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,19 +1,28 @@
-import React from "react";
-import { SafeAreaView, Text, FlatList } from "react-native";
-import {APP_NAME, API_KEY, COMPANY_EMAIL, API_URL} from '@env';
+import { SafeAreaView, Text, FlatList, ActivityIndicator} from "react-native";
+import {API_URL} from '@env';
+
+import ProductCard from "../../components/ProductCard";
+import useFetch from "../../components/hooks/useFetch";
 
 
 const Products = () => {
+    const {loading, data, error} = useFetch(API_URL)
+    console.log("render");
+    console.log({loading, data: data.length, error});
+    console.log('-----------------------');
 
-    const renderProduct = ({item}) => null;
+    const renderProduct = ({item}) => <ProductCard product={item} />;
+
+    if(error) {
+        return <Text>{error}</Text>
+    }
+
+    if(loading) {
+        return <ActivityIndicator size= "large" />;
+    }
     return (
         <SafeAreaView>
-            <Text> Products </Text>
-            <Text>My App name is {APP_NAME}</Text>
-            <Text>My API Key is {API_KEY}</Text>
-            <Text>Company email is {COMPANY_EMAIL}</Text>
-            <Text>My Url is {API_URL} </Text>
-            <FlatList data={null} renderItem={renderProduct}/>
+            <FlatList data={data} renderItem={renderProduct}/>
         </SafeAreaView>
     );
 };
